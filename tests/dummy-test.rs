0e482bffe576d93bb5e1d926d62a3b74377bf847
Vladimir Ulogov <vladimir.ulogov@me.com>
2023-03-21 15:13:40 -0600

    Initial commit

---
+++ tests/dummy-test.rs
@@ -0,0 +1,11 @@
+#![warn(unused_imports)]
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_dummy() {
+        assert_eq!(42, 42);
+    }
+}
---
+++ tests/enum-test.rs
@@ -0,0 +1,11 @@
+#![warn(unused_imports)]
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_enum() {
+        assert_eq!(42, 42);
+    }
+}
