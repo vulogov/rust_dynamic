e9803bfde21ad39b2f4b72f18e90f37122991c92
Vladimir Ulogov <vladimir.ulogov@me.com>
2023-03-29 18:11:16 -0600

    Dev #35

--- Documentation/RELEASE_NOTES_0.9.0.md
+++ Documentation/RELEASE_NOTES_0.9.0.md
@@ -1,3 +1,4 @@
 # What's new in 0.9.0
 
 * More test cases
+* Value.fmap coulf be applied to MAP/CONFIG/INFO/ASSOCIATION datatypes
--- src/lib.rs
+++ src/lib.rs
@@ -12,6 +12,7 @@ pub mod create_special;
 pub mod create_special_float;
 pub mod create_list;
 pub mod create_map;
+pub mod checker;
 pub mod dup;
 pub mod q;
 pub mod func;
