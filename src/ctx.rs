281b57ec78d1856783f390f5d97391a2c1902249
Vladimir Ulogov <vladimir.ulogov@me.com>
2023-03-30 22:45:49 -0600

    Dev #38

--- src/ctx.rs
+++ src/ctx.rs
@@ -1,11 +1,15 @@
-use crate::value::{Applicative, Value};
+use crate::value::{Value};
 
-pub type CtxAppFn  = fn(dyn Context,Value) -> Value;
+pub type CtxAppFn  = fn(&dyn Context,Value) -> Option<Value>;
+
+pub struct CtxApplicative {
+    pub f:      CtxAppFn,
+}
 
 pub trait Context {
-    fn new() -> Self;
-    fn resolve<N: AsRef<str>>(&self, name: N) -> Option<Applicative>;
-    fn get_association<N: AsRef<str>>(&self, name: N) -> Option<Value>;
+    fn new() -> Self where Self: Sized;
+    fn resolve(&self, name: &str) -> Option<CtxAppFn>;
+    fn get_association(&self, name: &str) -> Option<Value>;
 }
 
 pub struct NullContext {}
@@ -16,10 +20,21 @@ impl Context for NullContext {
 
         }
     }
-    fn resolve<N: AsRef<str>>(&self, _name: N) -> Option<Applicative> {
+    fn resolve(&self, _name: &str) -> Option<CtxAppFn> {
+        fn none_fn(_ctx: &dyn Context, _value: Value) -> Option<Value> {
+            None
+        }
+        Some(none_fn)
+    }
+    fn get_association(&self, _name: &str) -> Option<Value> {
         None
     }
-    fn get_association<N: AsRef<str>>(&self, _name: N) -> Option<Value> {
-        None
+}
+
+
+pub fn context(_ctx: impl Context, _name: &str, _value: Value) -> Option<Value> {
+    fn none_fun(_ctx: impl Context, _value: Value) -> Option<Value>{
+        Some(Value::none())
     }
+    none_fun(_ctx, _value)
 }
--- tests/ctx-test.rs
+++ tests/ctx-test.rs
@@ -2,13 +2,20 @@
 mod tests {
     #![allow(unused_imports)]
     use super::*;
-    use rust_dynamic::ctx::Context;
+    use rust_dynamic::value::Value;
+    use rust_dynamic::ctx::{Context, context};
     use rust_dynamic::ctx::{NullContext};
 
 
     #[test]
     fn test_null_ctx() {
         let ctx = NullContext::new();
-        assert!(ctx.resolve("hello").is_none());
+        assert!(ctx.get_association("hello").is_none());
+    }
+    #[test]
+    fn test_null_ctx_resolve() {
+        let ctx = NullContext::new();
+        let fun = ctx.resolve("test").expect("function expected");
+        assert!(fun(&ctx, Value::none()).is_none());
     }
 }
