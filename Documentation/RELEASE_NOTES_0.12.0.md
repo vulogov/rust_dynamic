7910fa7c0e4db45bf67de3f97d79d56282f41742
Vladimir Ulogov <vladimir.ulogov@me.com>
2023-04-05 01:20:08 -0600

    Dev #50

--- Documentation/RELEASE_NOTES_0.12.0.md
+++ Documentation/RELEASE_NOTES_0.12.0.md
@@ -4,3 +4,4 @@
 * Value.freduce(fun, Value) - reducing dynamic value to a single value
 * Value.freduce() supported for single-value and lists
 * Value.freduce() supported for MAP and METRICS
+* Value.as_json_value() converting Value to serde_json::value::Value
--- tests/json-test.rs
+++ tests/json-test.rs
@@ -23,4 +23,22 @@ mod tests {
         let json_out = data.to_json().unwrap();
         assert_ne!(json_out.len(), 0);
     }
+    #[test]
+    fn test_as_json_value_int() {
+        use serde_json;
+        let v = Value::from_int(42 as i64).as_json_value();
+        assert_eq!(serde_json::from_value::<i64>(v).unwrap(), 42 as i64);
+    }
+    #[test]
+    fn test_as_json_value_string() {
+        use serde_json;
+        let v = Value::from_string("hello").as_json_value();
+        assert_eq!(serde_json::from_value::<String>(v).unwrap(), "hello".to_string());
+    }
+    #[test]
+    fn test_as_json_value_list() {
+        use serde_json;
+        let v = Value::from_list(vec![Value::from_int(1), Value::from_int(2)]).as_json_value();
+        assert_eq!(v.as_array().unwrap().len(), 2);
+    }
 }
